bd5a2eeda3185db6e39a67d314b765ff92f0ea83
Alexander Ponomarev <alexander996@yandex.ru>
2016-01-17 09:17:00 +0100

    Ability to preprocess and load python files added
    Before only modules could be preprocessed and used, now
    files can be used too

--- shellpython/importer.py
+++ shellpython/importer.py
@@ -16,13 +16,26 @@ class PreprocessorImporter(object):
             module = import_module(module_name)
         except ImportError:
             spy_module_path = locator.locate_spy_module(module_name)
+            spy_file_path = locator.locate_spy_file(module_name)
+
             if spy_module_path is not None:
-                new_module_path = preprocessor.preprocess_module(spy_module_path)
-                new_module_pythonpath = os.path.split(new_module_path)[0]
-                if new_module_pythonpath not in sys.path:
-                    sys.path.append(new_module_pythonpath)
+                new_file_path = preprocessor.preprocess_module(spy_module_path)
+                new_file_pythonpath = os.path.split(new_file_path)[0]
+
+                if new_file_pythonpath not in sys.path:
+                    sys.path.append(new_file_pythonpath)
 
                 module = import_module(module_name)
+
+            elif spy_file_path is not None:
+                new_file_path = preprocessor.preprocess_file(spy_file_path, is_root_script=False)
+                new_file_pythonpath = os.path.split(new_file_path)[0]
+
+                if new_file_pythonpath not in sys.path:
+                    sys.path.append(new_file_pythonpath)
+
+                module = import_module(module_name)
+
             else:
                 raise
 
--- shellpython/locator.py
+++ shellpython/locator.py
@@ -4,9 +4,20 @@ import os.path
 
 def locate_spy_module(module_name):
     for python_path in sys.path:
+
         possible_module_path = os.path.join(python_path, module_name)
         if os.path.exists(possible_module_path):
             if os.path.exists(os.path.join(possible_module_path, '__init__.spy')):
                 return possible_module_path
+
+    return None
+
+
+def locate_spy_file(module_name):
+    for python_path in sys.path:
+
+        possible_file_path = os.path.join(python_path, module_name + '.spy')
+        if os.path.exists(possible_file_path):
+            return possible_file_path
 
     return None
