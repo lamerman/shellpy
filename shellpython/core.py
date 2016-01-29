d5f2b42e1faf81566a9eb68259bdf14d4d77ff79
Alexander Ponomarev <alexander996@yandex.ru>
2016-01-29 23:53:10 +0100

    Stderr added to Result

--- shellpython/core.py
+++ shellpython/core.py
@@ -12,18 +12,29 @@ class Result:
     You can compaire two results that will mean compaire of result strings
     """
     def __init__(self):
-        self.lines = []
+        self.stdout_lines = []
+        self.stderr_lines = []
         self.returncode = -1
 
-    def add_line(self, line):
+    def stdout_text(self):
+        return os.linesep.join(self.stdout_lines)
+
+    def stderr_text(self):
+        return os.linesep.join(self.stderr_lines)
+
+    def add_stdout_line(self, line):
         line_no_linesep = line[:-1] if len(line) > 0 and line[-1] == os.linesep else line
-        self.lines.append(line_no_linesep)
+        self.stdout_lines.append(line_no_linesep)
+
+    def add_stderr_line(self, line):
+        line_no_linesep = line[:-1] if len(line) > 0 and line[-1] == os.linesep else line
+        self.stderr_lines.append(line_no_linesep)
 
     def __str__(self):
-        return os.linesep.join(self.lines)
+        return self.stdout_text()
 
     def __iter__(self):
-        return iter(self.lines)
+        return iter(self.stdout_lines)
 
     def __eq__(self, other):
         return self.__str__() == other.__str__()
@@ -40,7 +51,7 @@ def exe(cmd, params):
     :param cmd: Command to execute
     :return: Result of execution
     """
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     result = Result()
 
@@ -48,12 +59,21 @@ def exe(cmd, params):
         if sys.version_info[0] == 3:
             line = line.decode(sys.stdout.encoding)
 
-        result.add_line(line)
+        result.add_stdout_line(line)
+
+    for line in p.stderr.readlines():
+        if sys.version_info[0] == 3:
+            line = line.decode(sys.stderr.encoding)
+
+        result.add_stderr_line(line)
 
     p.wait()
 
     if params.find('p') != -1:
-        print(result)
+        print(result.stdout_text())
+
+    if params.find('e') != -1:
+        print(result.stderr_text())
 
     result.returncode = p.returncode
 
