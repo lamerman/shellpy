1dcdb35ddcf494716d30745a9ebdf5ba6aab41c1
Alexander Ponomarev <alexander996@yandex.ru>
2016-02-01 23:24:23 +0100

    Description corrected in setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='shellpy',
-      version='0.5',
-      description='Python Distribution Utilities',
+      version='0.3.4',
+      description='A convenient tool for shell scripting in python',
       author='Alexander Ponomarev',
       author_email='alexander996@yandex.ru',
       url='https://github.com/lamerman/shellpy/',
-      download_url='https://github.com/lamerman/shellpy/tarball/0.3.3',
+      download_url='https://github.com/lamerman/shellpy/tarball/0.3.4',
       keywords=['shell', 'bash', 'sh'],
       packages=['shellpython'],
       scripts=['shellpy'],
