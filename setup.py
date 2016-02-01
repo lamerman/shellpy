389cafc8bbb0cd25c91e0d5b87cd88c2219f7cb2
Alexander Ponomarev <alexander996@yandex.ru>
2016-02-01 23:18:10 +0100

    Package name corrected in setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
-setup(name='Shell Python',
+setup(name='shellpy',
       version='0.5',
       description='Python Distribution Utilities',
       author='Alexander Ponomarev',
       author_email='alexander996@yandex.ru',
       url='https://github.com/lamerman/shellpython/',
-      download_url='https://github.com/lamerman/shellpython/0.3',
+      download_url='https://github.com/lamerman/shellpython/0.3.1',
       keywords=['shell', 'bash', 'sh'],
       packages=['shellpython'],
       scripts=['shellpy'],
       package_data={'shellpython': ['*.tpl']},
       install_requires=['colorama']
-     )
+      )
