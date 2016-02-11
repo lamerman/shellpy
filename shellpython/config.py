6bf8420fe565a70cd5f79c6f032a804eb9c51968
Alexander Ponomarev <alexander996@yandex.ru>
2016-02-11 23:11:30 +0100

    Description added to config constants

--- shellpython/config.py
+++ shellpython/config.py
@@ -1,5 +1,11 @@
+# prints all commands being executed
 PRINT_ALL_COMMANDS = False
+
+# prints stdout of every command executed
 PRINT_STDOUT_ALWAYS = False
+
+# every error (returncode != 0) in executed commands will make the script stop
 EXIT_ON_ERROR = False
-KEEP_LINESEP_IN_LINES = False
+
+# colorama is a plugin that makes output colored, this flag controls whether it is enabled
 COLORAMA_ENABLED = True
