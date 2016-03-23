1a76583194d44c386bed241948727a9c50ff969d
Alexander Ponomarev <alexander996@yandex.ru>
2016-03-23 08:57:29 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -16,12 +16,12 @@ except ImportError:
 
 
 setup(name='shellpy',
-      version='0.4.7',
+      version='0.5.0',
       description='A convenient tool for shell scripting in python',
       author='Alexander Ponomarev',
       author_email='alexander996@yandex.ru',
       url='https://github.com/lamerman/shellpy/',
-      download_url='https://github.com/lamerman/shellpy/tarball/0.4.7',
+      download_url='https://github.com/lamerman/shellpy/tarball/0.5.0',
       keywords=['shell', 'bash', 'sh'],
       packages=['shellpython'],
       package_data={'shellpython': ['*.tpl']},
